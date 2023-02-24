b72480a2af2cd7919b612fe221ed544f8f5a9bca
Gailius Besusparis <gbesusparis@digital.ai>
2023-02-24 13:43:04 +0200

    Add dropdowns and inputs

--- src/app/log-viewer/log-viewer.component.ts
+++ src/app/log-viewer/log-viewer.component.ts
@@ -1,24 +1,9 @@
 import { Component } from '@angular/core';
 
-
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
-
 @Component({
   selector: 'app-log-viewer',
   templateUrl: './log-viewer.component.html',
   styleUrls: ['./log-viewer.component.scss']
 })
 export class LogViewerComponent {
-
-  tiles: Tile[] = [
-    { text: 'One', cols: 1, rows: 1, color: 'lightblue' },
-    { text: 'Two', cols: 1, rows: 1, color: 'lightgreen' },
-    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
-    { text: 'Four', cols: 3, rows: 1, color: '#DDBDF1' },
-  ];
 }
--- src/app/log-viewer/log-viewer.module.ts
+++ src/app/log-viewer/log-viewer.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
-import { LogViewerComponent } from '../log-viewer/log-viewer.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatButtonModule } from '@angular/material/button';
 import { SharedModule } from '../shared/shared.module';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Components
+import { LogViewerComponent } from '../log-viewer/log-viewer.component';
+import { FiltersComponent } from '../filters/filters.component';
+
+// Material modules
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 import { LogAreaComponent } from './log-area/log-area.component';
 
 @NgModule({
-  declarations: [LogViewerComponent, LogAreaComponent],
-  imports: [MatGridListModule, MatButtonModule],
-  exports: [CommonModule, SharedModule, LogViewerComponent],
+  declarations: [LogViewerComponent, FiltersComponent, LogAreaComponent],
+  imports: [
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatInputModule
+  ],
+  exports: [CommonModule, SharedModule, LogViewerComponent, FiltersComponent]
 })
 export class LogViewerModule { }
