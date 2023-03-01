28d17e0aadde992102bd33eb4221c9e54ccf6c63
Eivydas Senkus <esenkus@digital.ai>
2023-03-01 11:21:17 +0200

    add custom context menu

--- src/app/log-viewer/log-area/log-area.component.ts
+++ src/app/log-viewer/log-area/log-area.component.ts
@@ -1,5 +1,15 @@
-import { Component, Input } from '@angular/core';
-import { LogLine } from '../domain/Interfaces';
+import {
+  Component,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output
+} from '@angular/core';
+import {
+  ContextMenuAction,
+  ContextMenuItem,
+  LogLine
+} from '../domain/Interfaces';
 
 @Component({
   selector: 'app-log-area',
@@ -9,4 +19,48 @@ import { LogLine } from '../domain/Interfaces';
 export class LogAreaComponent {
   @Input()
   logLines: LogLine[];
+
+  @Output() additionalLogKeyExcludeEvent = new EventEmitter<string>();
+
+  contextMenuItems: ContextMenuItem[] = [];
+  showContextMenu = false;
+  rightClickMenuPositionX: number;
+  rightClickMenuPositionY: number;
+
+  @HostListener('document:click')
+  public documentClick(): void {
+    this.showContextMenu = false;
+  }
+
+  public contextMenuItemSelected(contextMenuItem: ContextMenuItem) {
+    switch (contextMenuItem.action) {
+      case ContextMenuAction.excludeKey:
+        const logLine = contextMenuItem.data as LogLine;
+        this.additionalLogKeyExcludeEvent.emit(logLine.logKey);
+    }
+  }
+
+  public displayContextMenu(event, logLine: LogLine) {
+    this.showContextMenu = true;
+
+    this.contextMenuItems = [
+      {
+        action: ContextMenuAction.excludeKey,
+        text: `Filter out '${logLine.logKey}' log key`,
+        data: logLine
+      }
+    ];
+
+    this.rightClickMenuPositionX = event.clientX;
+    this.rightClickMenuPositionY = event.clientY;
+  }
+
+  public getContextMenuStyle() {
+    return {
+      position: 'fixed',
+      'z-index': 2,
+      left: `${this.rightClickMenuPositionX}px`,
+      top: `${this.rightClickMenuPositionY}px`
+    };
+  }
 }
